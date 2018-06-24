57087a818502c5b6274c8f4d7158a742e5d07122
CrookiNari <38683641+CrookiNari@users.noreply.github.com>
2018-06-24 21:43:36 +1000

    Add files via upload

--- maps/Plain of Andor/ag20.lua
+++ maps/Plain of Andor/ag20.lua
@@ -12,9 +12,20 @@ local game = map:get_game()
 
 -- Event called at initialization time, as soon as this map becomes is loaded.
 function map:on_started()
+local movement = sol.movement.create("path")
+  movement:set_path{4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,0,0}
+  movement:set_speed(20)
+  movement:set_loop(true)
+  movement:start(traveller2)
+
+end
 
   -- You can initialize the movement and sprites of various
   -- map entities here.
+
+function traveller2:on_interaction()
+game:start_dialog ("plains_traveller")
+
 end
 
 -- Event called after the opening transition effect of the map,
